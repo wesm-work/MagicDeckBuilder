41dd86274a6b52bf9aaa3008977f94e36ba02df9
Wes Mincic <84863857+wesm-work@users.noreply.github.com>
2026-01-04 14:23:46 -0600

    Initial project setup for MagicDeckBuilder app
    Added Xcode project files, asset catalogs, and initial SwiftUI app structure with ContentView and MagicDeckBuilderApp. This sets up the basic iOS application foundation.

---
+++ MagicDeckBuilder/MagicDeckBuilder/MagicDeckBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicDeckBuilderApp.swift
+//  MagicDeckBuilder
+//
+//  Created by Wes Mincic on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct MagicDeckBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
